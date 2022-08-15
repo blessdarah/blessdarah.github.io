07d45d998dfde640f1174f462838854807b419c3
= <blessdarahuba@gmail.com>
2022-08-15 01:06:25 +0100

    config: Add tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>Welcome to Darah-B Studios</div>
+        <>
+            <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
+                button
+            </button>
+            <div>Welcome to Darah-B Studios</div>
+        </>
     )
 }
 export default App;
